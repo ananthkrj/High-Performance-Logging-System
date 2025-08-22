d92723bad2b03654bf22fd7844037113dcbb9675
Ananth <ananthkrj@gmail.com>
2025-08-22 12:31:54 -0700

    constructor updated, need to test, and need to implement destructor

--- include/spsc_queue.hpp
+++ include/spsc_queue.hpp
@@ -6,6 +6,7 @@
 // lock free single producer, a single consumer circular buffer using 
 // atomic operations and memory ordering semantics
 #include <concepts>
+#include <new>
 
 // std::memory_order semantics quite important and atomics
 
@@ -16,8 +17,9 @@
 // Concept (optional): Used in c++20 implementation
 // manipulating std libraries importing in
 
-class SPSCQueue {
-private:
+// template declaration for class
+
+template <typename T, typename Allocator = std::allocator<T>> class SPSCQueue {
 
 /*
 populate with functions i will need regardless
@@ -39,6 +41,32 @@ properties of types at compile time, (need to do this for Args)
 */
 public:
     // constructor (will be allocating here)
+    explicit spsc_queue(const size_t capacity, const Allocator *allocator = Allocator()) 
+    : capacity_(capacity), allocator_(allocator) {
+
+        // capacity of queue needs to hae something
+        if (capacity_ < 1) {
+            capacity_ = 1;
+        }
+        capacity_++;
+
+        // prevent the overflowing of size_t, needs to be equal to this size
+        if (capacity_ < SIZE_MAX - 2 * kPadding) {
+            capacity_ SIZE_MAX - 2 * kPadding;
+        }
+
+        // add macro check for allocator at least and if constexpr
+
+            auto res = allocator_.allocator_at_least(capacity_ + 2 * kPadding);
+            slots_ = res.ptr;
+            capacity_ = res.count - 2 * kPadding;
+
+            // allows containers to interact with any allocator type
+            slots_ = std::allocator_traits<Allocate>::allocate(allocator_, capacity_ + 2 * kPadding);
+
+            // asserts and tests
+    
+    }
 
     // destructor
 
@@ -60,8 +88,8 @@ public:
 
         // next write index is equal to the capacity, need to define _capacity
         // placeholder for now
-        while (nextWriteIndex == capacity_) {
-            nextIndex = 0;
+        while (nextWriteIndex == capacity_) { 
+            nextWriteIndex = 0;
         }
 
         // Need to create readIndexCache in private access modifier
@@ -69,10 +97,16 @@ public:
             readIndexCache_ = readIndex.load(std::memory_order_acquire);
         }
 
+        // allocating new memory, pass in address of slots, writeIndex + kPadding
+        // 
 
+    }
 
-
-        
+    template <typename... Args>
+    void force_emplace(Args&&... args) noexcept(std::is_nothrow_constructible<T, Args&&...>::value)
+    {
+        static_assert(std::is_constructible<T, Args&&...>::value,
+            "T must be constructible with Args&&...");
     }
 
     template <typename... Args>
@@ -83,14 +117,12 @@ public:
 
     }
 
-    template <typename... Args>
-    void push_emplace(Args&&... args) noexcept(std::is_nothrow_constructible<T, Args&&...>::value)
-    {
-        static_assert(std::is_constructible<T, Args&&...>::value,
-            "T must be constructible with Args&&...");
-    }
 
     void push() {
+
+    }
+
+    void force_push() {
 
     }
 
@@ -98,9 +130,6 @@ public:
 
     }
 
-    void force_push() {
-
-    }
 
     void pop() {
 
@@ -111,5 +140,59 @@ public:
     }
 
     // all other functions are size or empty functions/getters/setters
- 
+    // [[nodiscard]], return value of function should not be ignored
+    [[nodiscard]] size_t size() {
+
+    }
+
+    [[nodiscard]] bool empty() {
+
+    }
+
+    [[nodsicard]] size_t capacity() {
+
+    }
+
+
+private:
+    // need to define kCacheLineSize and kPadding
+    // std::hardware_destructive_inference_size represents
+    // the min offset between two objects in a multi threaded
+    // environment to prevent "false sharing"
+
+    // macro to check if feature exists
+#if defined(__cpp_lib_hardware_destructive_interference_size)
+    static constexpr size_t kCacheLineSize = 
+        std::hardware_destructive_interference_size;
+#else
+    // Fallback for older compilers or those without support
+    static constexpr size_t kCacheLineSize = 64; 
+#endif
+
+    // padding to avoid false sharing between slots_ and adjacent locations
+    // find out what is wrong here
+    static constexpr size_t kPadding = (kCacheLineSize - 1) / sizeof(T) + 1; 
+
+    size_t capacity;
+    T *slots_;
+    // define allocator as well
+    #if defined(__has_cpp_attribute) && __has_cpp_attribute(no_unique_address)
+        Allocator allocator_ [[no_unique_address]];
+    #else 
+        Allocator allocator_;
+    #endif
+
+    // align these variables to kCacheLineSize to avoid false sharing
+    // all idxcache operations
+    // alignas controls memory alignment of variables
+
+    // types of variables, initlize all to 0 for initial state
+    // atomic, 
+    alignas(KCacheLineSize) std::atomic<size_t> readIndex_ = {0};
+    // size_t
+    alignas(KCacheLineSize) size_t readIndexCache_ = 0;
+    // atomic
+    alignas(KCacheLineSize) std::atomic<size_t> writeIndex_ = {0};
+    // size_t
+    alignas(KCacheLineSize) size_t writeIndexCache_ = 0;
 };
