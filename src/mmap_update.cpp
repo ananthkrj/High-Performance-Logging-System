e6bf6368ad54f772cf991ac0338f7169f8554fd1
Ananth <ananthkrj@gmail.com>
2025-09-09 16:41:02 -0700

    finished mmap update, need to test, but to test need to be able to compile. So situate this in makefile. Means make makefile is next file to work on

--- src/mmap_update.cpp
+++ src/mmap_update.cpp
@@ -24,31 +24,42 @@ int main(int argc, char *argv[]) {
     }
 
     // initialize length of arguments
+    size_t length = strlen(argv[2]) + 1;
 
     // declare fd (file descriptor variable using args, O_RDWR, and the linux permission)
+    int fd = open(argv[2], O_RDWR, PERM);
 
     // use fstat to get size of the file
     fstat(fd, &fileStatus);
     // then call filestatus.st_size in fsize variable declaration
+    size_t fsize = fileStatus.st_size;
     
     // print size of file args
+    std::cout << "File : " << argv[1] << " size is" << length << ":" << fsize << std::endl;
 
-    // mmap operation with proper flags into addr variable
+    // mmap operation with proper flags into addr variable 
+    // need to pass memory address (let compiler decide, size, prot read, and 
+    // write flags, map shared flag fd, then the offsert which is 0)
+    void *addr = mmap(addr, fsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
     // print read addre vsraible
+    std::cout << (char*)addr << std::endl;
 
     // memcpy 
+    // copy address and arguments, then pass in length of the arguments
+    memcpy(addr, argv[2], strlen(argv[2]) + 1);
+    // print after copy, print the type casted addr
+    std::cout << (char*)addr << std::endl;
 
-    // print after copy
-
-    // sync
+    // sync addr fisze, (writing changes back to the disk)
+    msync(addr, fsize, MS_SYNC);
 
     // unmap
+    munmap(addr, fsize);
 
     // close
+    close(fd);
 
     // return succes
-
-
-
+    return EXIT_SUCCESS;
 }
