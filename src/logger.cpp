17c2b5a38a8dd6584e8edb9b365e7837f765bdd3
Ananth <ananthkrj@gmail.com>
2025-09-09 22:24:39 -0700

    Plan for logger, expand on this tmrrw in detail and start coding up file

--- src/logger.cpp
+++ src/logger.cpp
@@ -2,3 +2,19 @@
 // error recovery, and thread management
 
 // Why important: it is the system orchestrator
+
+/*
+core components will contain:
+
+threading model
+
+message format
+
+pipeline integration (tieing in other components)
+
+configuration and control
+
+simd integration points
+
+error handling
+*/
